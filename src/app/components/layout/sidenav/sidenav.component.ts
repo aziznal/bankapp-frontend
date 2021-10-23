0bcd528ee20a7ce4e01c0f23126956e7d2222bf6
aziznal <aziznal.dev@gmail.com>
2021-10-23 20:59:51 +0300

    refactor: remove redundant logout button from sidenav

--- src/app/components/layout/sidenav/sidenav.component.ts
+++ src/app/components/layout/sidenav/sidenav.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoginService } from 'src/app/pages/login-page/services/login.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,10 +6,5 @@ import { LoginService } from 'src/app/pages/login-page/services/login.service';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-  constructor(private loginService: LoginService, private router: Router) {}
-
-  logout() {
-    this.loginService.logout();
-    this.router.navigate(['/login']);
-  }
+  constructor() {}
 }
